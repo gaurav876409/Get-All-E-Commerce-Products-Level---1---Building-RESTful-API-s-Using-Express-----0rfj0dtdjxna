6ecf5562f75e9e120b5874871483f7a02c74848f
gaurav876409 <gaurav876409@gmail.com>
2023-07-15 23:13:48 +0530

    up

--- src/app.js
+++ src/app.js
@@ -10,5 +10,16 @@ app.use(express.json());
 
 // Write GET endpoint for sending all the products to client here
 // Endpoint - /api/v1/products
+app.get("/products", (req, res) => {
+    if(products){
+        res.status(200).json({
+            status: "success",
+            message: "Product fetched successfully",
+            data: products
+        });
+    }else{
+        res.status(404).json({message: "product not found"});
+    }
+});
 
 module.exports = app;
